8d338c8202b8fb51d760591a597bb1a2214bca2d
jeyde <jeydensj@gmail.com>
2026-04-25 00:13:05 -0400

    Finish the code for turning a file into Hex values

--- AES-Algorithm/src/Main.java
+++ AES-Algorithm/src/Main.java
@@ -1,4 +1,24 @@
+import java.util.HexFormat;
+import java.nio.file.*;
+import java.util.Scanner;
+
 void main() {
-    System.out.println("sup guys");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Copy and paste your file path");
+    String uInput = sc.nextLine();
+    Path myAesFile = Paths.get(uInput);
+    System.out.println(myAesFile);
+    try {
+        byte [] fileToBytes = Files.readAllBytes(myAesFile);
+        HexFormat hex = HexFormat.ofDelimiter(" ");
+        String plaintext = hex.formatHex(fileToBytes);
+        System.out.println("The hex values of our plaintext is: "+plaintext+"\n");
+
+    }catch (IOException e){
+        System.out.println(e.getMessage());
+    }
+
+
+
 
 }
