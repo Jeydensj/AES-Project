a3ff52a4caf9d56740f2dfff22961555d34cdef5
Juan Ferreira <nanoferreirag@gmail.com>
2026-04-29 19:00:14 -0400

    merging branches and fixing merge conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 
-=======
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HexFormat;
->>>>>>> origin/Jeyden-Sandbox
 import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,9 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HexFormat;
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-=======
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Copy and paste your file path:");
@@ -34,23 +24,6 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Could not read the file. Check the path!");
         }
-    }
->>>>>>> origin/Jeyden-Sandbox
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Copy and paste your file path");
-        String uInput = sc.nextLine();
-        Path myAesFile = Paths.get(uInput);
-        System.out.println(myAesFile);
-
-        try {
-            byte[] fileToBytes = Files.readAllBytes(myAesFile);
-            HexFormat hex = HexFormat.ofDelimiter(" ");
-            String plaintext = hex.formatHex(fileToBytes);
-            System.out.println("The hex values of our plaintext is: " + plaintext + "\n");
-        } catch (IOException var7) {
-            IOException e = var7;
-            System.out.println(e.getMessage());
-        }
     }
 }
