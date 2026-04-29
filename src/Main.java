3b0090281520a158f61cf692821cf435ac00bbea
jeyde <jeydensj@gmail.com>
2026-04-29 06:11:02 -0400

    cleaned up files

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HexFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Copy and paste your file path:");
+        String uInput = sc.nextLine();
+// 1. Convert the input string into a Path
+        Path path = Paths.get(uInput);
+        try {
+            // 2. Read all bytes from the file
+            byte[] fileBytes = Files.readAllBytes(path);
+            // 3. Convert those bytes to Hex
+            HexFormat hex = HexFormat.ofDelimiter(" ");
+            String hexOutput = hex.formatHex(fileBytes);
+
+            System.out.println("Hex Output: " + hexOutput);
+        } catch (IOException e) {
+            System.out.println("Could not read the file. Check the path!");
+        }
     }
 
 }
