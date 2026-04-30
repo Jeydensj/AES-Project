fb3ab041ddcff6d4e6b317bc9e735fa4e2f47f11
jeyde <jeydensj@gmail.com>
2026-04-29 21:35:26 -0400

    finished mix columns

--- src/MixColumns.java
+++ src/MixColumns.java
@@ -6,28 +6,61 @@ public class MixColumns {
         {0x03, 0x01, 0x01, 0x02}
     };
 
-    public static int gfMultiply(byte[][] a, byte[][] b) {
+    byte [][] inverseMBox={
+            {0x0E, 0x0B, 0x0D, 0x09},
+            {0x09, 0x0E, 0x0B, 0x0D},
+            {0x0D, 0x09, 0x0E, 0x0B},
+            {0x0B, 0x0D, 0x09, 0x0E}
+    };
+
+    private byte gfMultiply(byte a, byte b) {
         int p = 0;
+        int a_int = a & 0xFF; // Treat as unsigned
+        int b_int = b & 0xFF; // Treat as unsigned
+
         for (int i = 0; i < 8; i++) {
-            if ((b & 1) != 0) {
-                p ^= a;
+            if ((b_int & 1) != 0) {
+                p ^= a_int;
             }
-            boolean hiBitSet = (a & 0x80) != 0;
-            a <<= 1;
+            boolean hiBitSet = (a_int & 0x80) != 0;
+            a_int <<= 1;
             if (hiBitSet) {
-                a ^= 0x11B; // AES irreducible polynomial
+                a_int ^= 0x1B; // The AES polynomial
             }
-            b >>= 1;
+            b_int >>= 1;
         }
-        return p & 0xFF;
+        return (byte) (p & 0xFF);
     }
 
     public byte[][] mixingColumns(byte[][] shiftRowsTable){
-        byte[][] temp = new byte[4][4];
-        for(int i = 0;i<4;i++){
-            for(int j =0; j<4;j++){
-                temp[i][j] =
+        byte[][] sPrimeBox = new byte[4][4];
+        int SRTrow = 4;
+        int mBoxRow = 4;
+        int mBoxCol = 4;
+        for(int i = 0;i<SRTrow;i++){
+            for(int j =0; j<mBoxCol;j++){
+                byte num = 0;
+                for (int k = 0; k<mBoxRow;k++) {
+                    num ^= gfMultiply(mBox[j][k], shiftRowsTable[k][i]);
+                }
+                sPrimeBox[i][j] = num;
             }
         }
+        return sPrimeBox;
     }
+
+    public byte[][] inverseMixingColumns(byte[][] shiftRowsTable){
+        byte[][] inverseSPrimeBox = new byte[4][4];
+        for(int i = 0;i<4;i++){
+            for(int j =0; j<4;j++){
+                byte num = 0;
+                for (int k = 0; k<4;k++) {
+                    num ^= gfMultiply(inverseMBox[j][k], shiftRowsTable[k][i]);
+                }
+                inverseSPrimeBox[i][j] = num;
+            }
+        }
+        return inverseSPrimeBox;
+    }
+
 }
