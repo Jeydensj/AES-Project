d1a50d05532fa2f10f31618f520589baedacd27f
jeyde <jeydensj@gmail.com>
2026-04-29 22:29:50 -0400

    updating my main

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,38 @@ public class Main {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Copy and paste your file path:");
-        String uInput = sc.nextLine();
-// 1. Convert the input string into a Path
-        Path path = Paths.get(uInput);
-        try {
-            // 2. Read all bytes from the file
-            byte[] fileBytes = Files.readAllBytes(path);
-            // 3. Convert those bytes to Hex
-            HexFormat hex = HexFormat.ofDelimiter(" ");
-            String hexOutput = hex.formatHex(fileBytes);
 
-            System.out.println("Hex Output: " + hexOutput);
-        } catch (IOException e) {
-            System.out.println("Could not read the file. Check the path!");
+        System.out.println("Copy and paste your file path");
+        String uInput = sc.nextLine();
+
+        System.out.println("User should input key: ");
+        String keyString = sc.nextLine();
+        byte[] keyArray = keyString.getBytes();
+        if(keyArray.length<16){ // if key size < 16
+            byte[] padded = new byte[16]; // new byte[] with padding
+            for(int i=0;i<16;i++){
+                if(i< keyArray.length){
+                    padded[i]=keyArray[i]; // storing key until its last byte
+                }
+                else{
+                    padded[i]=0x20; // padding with <space> after last byte
+                }
+            }
+            keyArray=padded; // updating the keyArray[]
+        }
+        Path myAesFile = Paths.get(uInput);
+        System.out.println(myAesFile);
+
+        try {
+            byte[] fileToBytes = Files.readAllBytes(myAesFile);
+            HexFormat hex = HexFormat.ofDelimiter(" ");
+            String plaintext = hex.formatHex(fileToBytes);
+            System.out.println("The hex values of our plaintext is: " + plaintext + "\n");
+
+
+        } catch (IOException var7) {
+            IOException e = var7;
+            System.out.println(e.getMessage());
         }
 
     }
