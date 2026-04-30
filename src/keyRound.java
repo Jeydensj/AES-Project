ec8dfa1e78b131bf8e4211abf9c07530a8268e81
efeakbas1 <ahmetefeakbas9@gmail.com>
2026-04-29 20:35:36 -0400

    DONE SubWord using substitute_math(...)

--- src/keyRound.java
+++ src/keyRound.java
@@ -31,7 +31,7 @@ public class keyRound {
             }
 
             if(i % 4 == 0){
-                temp=gFunc(temp, i/4); // 2.2, WILL WORK ON IT
+                temp=gFunc(temp, i/4, key); // 2.2, determining Wi-1, if (i mod 4=0)
             }
             for(int r=0;r<keyArray.length;r++){ // THIRD, Wi= Wi-4 XOR temp(Wi-1)
                 keyArray[r][i]=(byte)(keyArray[r][i-4] ^ temp[r]);
@@ -39,7 +39,7 @@ public class keyRound {
         }
 
     }
-    public byte[] gFunc(byte[] word, int round){
+    public byte[] gFunc(byte[] word, int round, byte[] key){
         byte[] cloneWord = word.clone();
         // FIRST, RotWord
         byte temp = cloneWord[0];
@@ -49,7 +49,26 @@ public class keyRound {
         cloneWord[3]=temp;
 
         // SECOND, SubWord
+        byte[][] byteKey = new byte[4][4]; // key --> from 1D to 2D
+        for(int i=0;i<key.length;i++){
+            int col= i/4;
+            int row= i%4;
+            byteKey[row][col]=key[i];
+        }
+        int[][] intKey = new int[byteKey.length][byteKey[0].length]; // byte[][]-->int[][]
+        for(int i=0;i<byteKey.length;i++){
+            for(int j=0; j<byteKey[0].length;j++){
+                intKey[i][j]=byteKey[i][j] & 0xFF; // bytes are getting converted to ints
+            }
+        }
+        int[][] SBOX = Substitution.substitute_math(intKey,true);
         byte[] subWord=cloneWord;
+        for(int i=0; i< subWord.length;i++){
+            int val = cloneWord[i] & 0xFF;
+            int row = (val>>4) & 0x0F;
+            int col = val & 0x0F;
+            subWord[i]=(byte)SBOX[row][col];
+        }
         // THIRD, XOR w/ Rcon[round]
         byte[] newWord=new byte[4];
         for(int r=0; r< subWord.length;r++){
