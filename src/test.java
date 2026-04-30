bfbd426e434989d7cbbcd2d1d85a0fb8fd724573
jeyde <jeydensj@gmail.com>
2026-04-29 22:15:57 -0400

    some minor bug fixing

--- src/test.java
+++ src/test.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 
 public class test{
     public static void main(String[] args) {
-
-
+        ShiftRows sr = new ShiftRows();
+        MixColumns mc = new MixColumns();
         Scanner sc = new Scanner(System.in);
-        int[][] test1 = {
+        byte[][] test1 = {
                 {0x00, 0x00, 0x01, 0x01},
                 {0x03, 0x03, 0x07, 0x07},
                 {0x0f, 0x0f, 0x1f, 0x1f},
                 {0x3f, 0x3f, 0x7f, 0x7f}
         };
-        int[][] output1 = Substitution.substitute_math(test1, true);
-        int[][] output2 = Substitution.substitute_table(test1, true);
+        byte[][] output1 = mc.inverseMixingColumns(test1);
+        //int[][] output2 = Substitution.substitute_table(test1, true);
 
-        for (int[] row : output1) {
+        for (byte[] row : output1) {
             for (int b : row) {
                 System.out.print(Integer.toHexString(b) + ", ");
             }
             System.out.println();
         }
         System.out.println();
-        for (int[] row : output2) {
+        /*for (int[] row : output2) {
             for (int b : row) {
                 System.out.print(Integer.toHexString(b) + ", ");
             }
             System.out.println();
-        }
+        }*/
     }
 }
