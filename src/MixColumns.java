ee60a111c934582a4b72e7caa46c5c4604495125
jeyde <jeydensj@gmail.com>
2026-04-29 20:41:56 -0400

    added some more changes

--- src/MixColumns.java
+++ src/MixColumns.java
@@ -1,3 +1,33 @@
 public class MixColumns {
+    byte[][] mBox = {
+        {0x02, 0x03, 0x01, 0x01},
+        {0x01, 0x02, 0x03, 0x01},
+        {0x01, 0x01, 0x02, 0x03},
+        {0x03, 0x01, 0x01, 0x02}
+    };
 
+    public static int gfMultiply(byte[][] a, byte[][] b) {
+        int p = 0;
+        for (int i = 0; i < 8; i++) {
+            if ((b & 1) != 0) {
+                p ^= a;
+            }
+            boolean hiBitSet = (a & 0x80) != 0;
+            a <<= 1;
+            if (hiBitSet) {
+                a ^= 0x11B; // AES irreducible polynomial
+            }
+            b >>= 1;
+        }
+        return p & 0xFF;
+    }
+
+    public byte[][] mixingColumns(byte[][] shiftRowsTable){
+        byte[][] temp = new byte[4][4];
+        for(int i = 0;i<4;i++){
+            for(int j =0; j<4;j++){
+                temp[i][j] =
+            }
+        }
+    }
 }
--- src/ShiftRows.java
+++ src/ShiftRows.java
@@ -4,6 +4,7 @@ public class ShiftRows {
         byte[][] temp = new byte[4][4];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
+                // modding by 4 helps with wrapping around and the first row is left alone
                 temp[i][j] = sBoxTable[i][(j + i) % 4];
             }
         }
