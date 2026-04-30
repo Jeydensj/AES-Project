09b81ea50cee5c86b2133d9acaf689d1726e8f21
jeyde <jeydensj@gmail.com>
2026-04-29 20:03:13 -0400

    added both the shiftrows and inerseshiftrows function

--- src/ShiftRows.java
+++ src/ShiftRows.java
@@ -1,10 +1,20 @@
 
 public class ShiftRows {
-    public byte[][] shiftingRows(byte[][] sBoxTable) {
+    public byte[][] shiftingRows(byte[][] sBoxTable, boolean encrypt) {
         byte[][] temp = new byte[4][4];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 temp[i][j] = sBoxTable[i][(j + i) % 4];
+            }
+        }
+        return temp;
+    }
+
+    public byte[][] inverseShiftingRows(byte[][] sBoxTable, boolean encrypt) {
+        byte[][] temp = new byte[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                temp[i][j] = sBoxTable[i][(j - i) % 4];
             }
         }
         return temp;
