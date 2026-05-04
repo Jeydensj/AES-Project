982f56ae609b2e557351ce6bc03f89c6c50a955d
jeyde <jeydensj@gmail.com>
2026-05-04 13:32:56 -0400

    changes

--- src/test.java
+++ src/test.java
@@ -11,8 +11,9 @@ public class test{
                 {0x0f, 0x0f, 0x1f, 0x1f},
                 {0x3f, 0x3f, 0x7f, 0x7f}
         };
-        byte[][] output1 = mc.inverseMixingColumns(test1);
-        //int[][] output2 = Substitution.substitute_table(test1, true);
+
+        byte[][] output1 = sr.shiftingRows(test1);
+        byte[][] output2 = mc.mixingColumns(test1);
 
         for (byte[] row : output1) {
             for (int b : row) {
@@ -21,11 +22,11 @@ public class test{
             System.out.println();
         }
         System.out.println();
-        /*for (int[] row : output2) {
+        for (byte[] row : output2) {
             for (int b : row) {
                 System.out.print(Integer.toHexString(b) + ", ");
             }
             System.out.println();
-        }*/
+        }
     }
 }
